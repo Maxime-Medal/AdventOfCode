f1db43f9490cdcb1dad62221f8c541ed6bba78ae
Maxime MÉDAL <medal.max@gmail.com>
2024-05-12 21:35:06 +0200

    add day2.js

--- day2.js
+++ day2.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
 // In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
 
 // The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
@@ -23,53 +24,54 @@ const maxRed = 12;
 const maxGreen = 13;
 const maxBlue = 14;
 
-let red = 0;
-let green = 0;
-let blue = 0;
 
-let result = 0;
+let result = [];
 
-const extractInt = (string) => {
-  const regex = /[\D]/g;
-  return parseInt(string.replace(regex, ''));
-}
 
 // premiere boucle pour extraire le N° du game et le set
-for (let a = 0; a < lines.length; a++) {
-  let game = lines[a].split(':')
-  let gameNum = extractInt(game[0]);
+lines.map(line => {
+
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+
+  let game = line.split(':');
+  let gameNum = parseInt(game[0].split(" ")[1]);
   let gameSet = game[1].split(";");
 
+  // console.log("gameSet", gameSet);
   // boucle sur le set [ ' 1 blue, 2 green', ' 3 green, 4 blue, 1 red', ' 1 green, 1 blue' ]
   for (let i = 0; i < gameSet.length; i++) {
     const ga = gameSet[i].split(',');
-
+    // console.log("ga", ga);
     for (let g = 0; g < ga.length; g++) {
       const color = ga[g];
-      // console.log("color", color);
-      if (color.includes("blue")) {
-        blue += extractInt(color);
-      }
-      if (color.includes("green")) {
-        green += extractInt(color);
-      }
-      if (color.includes("red")) {
-        red += extractInt(color);
+      console.log("color", color);
+      if (color.match(/blue/)) {
+        blue += parseInt(color);
+        // console.log("blue", blue);
+      } else if (color.match(/green/)) {
+        green += parseInt(color);
+        // console.log("green", green);
+      } else if (color.match(/red/)) {
+        red += parseInt(color);
+        // console.log("red", red);
       }
     };
   }
-
   if (!(blue > maxBlue || green > maxGreen || red > maxRed)) {
-    result += gameNum
+    // console.log(typeof gameNum);
+    result.push(gameNum);
   }
-  blue = 0
-  green = 0
-  red = 0
+  // console.log("-----------");
 
-};
+})
 
 console.log("result", result);
+console.log("sum", result.reduce((a, b) => a + b));
 
+
+// HELP
 
 // function possibleGames(data, red, green, blue) {
 //   const possibleGames = [];
@@ -111,5 +113,5 @@ console.log("result", result);
 // ];
 
 // const result1 = possibleGames(lines, 12, 13, 14);
-// console.log("Possible Games:", result);
+// console.log("Possible Games:", result1);
 // console.log("Sum of IDs:", result1.reduce((sum, id) => sum + id, 0));
